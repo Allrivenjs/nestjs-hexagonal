5d3383901ee76424a4105c0c8931835cf830fe6a
allrivenjs <jaimeruizxdd2@gmail.com>
2024-07-31 16:59:59 -0500

    Swagger Doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from './infraestructure/shared/config/cors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalFilters()
+
+  app.enableCors(CORS);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API Documentation Store')
+    .setDescription(
+      'API Documentation Store, where you can find all the information about the endpoints',
+    )
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
